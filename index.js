2647d6a996a6bc57d323caafd92ca20beb6ad0f6
emonShimoul <emonshimoul@gmail.com>
2022-07-23 12:44:04 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log("connected to mongodb");
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
